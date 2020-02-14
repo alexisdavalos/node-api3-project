ba0b4f68638e0b1dae2cfb47a40148f74f067a97
Alexis <alexisdavalos.tech@gmail.com>
2020-02-13 20:06:13 -0500

    Project Update

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
-
+const helmet = require('helmet');
+const cors = require('cors')
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+
+const userDb = require('./users/userDb');
+const postDb = require('./posts/postDb');
+
+//middleware
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+// routes - endpoints
+server.use("/api/user", logger, userRouter);
+server.use("/api/post", postRouter);
+
+// default base url http://localhost:5000 from index.js
+server.get('/', logger,  (req, res) => {
+  res.send(`<h2>Node API-III Project</h2>`);
 });
+
+module.exports = server;
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  (req.params.id) ? 
+  console.log(`${req.method} Request to ${req.originalUrl} at: ${Date.now()} includes id:${req.params.id}`) :
+  console.log(`${req.method} Request to ${req.originalUrl} at: ${Date.now()} `); //requests url of request method
+  next(); //moves on
+}
 
-module.exports = server;
